001c333273883e9c33bcbe75e791d720f83ebc03
M Coindreau <mariannacoindreau@gmail.com>
2017-10-03 10:39:25 -0500

    Final fix

--- src/Author.java
+++ src/Author.java
@@ -5,25 +5,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList<String>();
+        books = new ArrayList<String>(); //when assigning a variable can leave out type
     }
 
     /**
      * @deprecated Use publishedBooks instead
      */
-    @SuppressWarnings({"deprecation", "unchecked"})
+//    @SuppressWarnings({"deprecation", "unchecked"})
     public List<String> getBooks() {
         return books;
     }
-    @SuppressWarnings("unchecked") //pass in unchecked warning to remove error
+//    @SuppressWarnings("unchecked") //pass in unchecked warning to remove error
     public List<String> publishedBooks() {
         return books;
     }
-    @SuppressWarnings("unchecked")
+//    @SuppressWarnings("unchecked")
     public void addBook(String book) {
         books.add(book);
     }
